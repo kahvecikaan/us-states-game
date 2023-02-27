57170bc8748bd5c20c80b80664eaaa6b893dca63
kaankahveci <kaankahveci29@outlook.com>
2023-02-27 12:36:31 +0300

    list comprehension to shorten the code

--- main.py
+++ main.py
@@ -18,10 +18,7 @@ while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50 Correct",
                                     prompt="What's another state name? ").title()
     if answer_state == "Exit":
-        missing_states = []
-        for state in all_states:
-            if state not in guessed_states:
-                missing_states.append(state)
+        missing_states = [state for state in all_states if state not in guessed_states]
         new_data = pandas.DataFrame(missing_states)
         new_data.to_csv("states_to_learn.csv")
         break
@@ -44,5 +41,3 @@ screen.exitonclick()
 #
 # turtle.onscreenclick(get_mouse_click_coor)
 # turtle.mainloop()
-
-
